66f7c699a3451b8db165f1749d4cc48a11bf90a2
shahar <shaharco99@gmail.com>
2024-04-23 17:10:21 +0300

    change test file for docker compose

--- WorldOfGames/test.py
+++ WorldOfGames/test.py
@@ -1,15 +1,14 @@
-import os
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def test_scores_service():
-    url_score = "http://192.168.49.2:5000/"
+    url_score = "http://score:5000"  # Assuming 'web' is the service name of your web application container
     options = Options()
     options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    driver = webdriver.Remote("http://192.168.49.2:4444", options=options)
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", options=options)  # Connect to Selenium container
     driver.get(url_score)
     score = int(driver.find_element(By.ID, "score").text)
     if 0 <= score <= 1000:
@@ -20,11 +19,3 @@ def test_scores_service():
         assert False
 
 test_scores_service()
-
-# def main_function():
-#     n = 0
-#     if test_scores_service():
-#         return sys.exit(n)
-#     else:
-#         n = "-1"
-#         return sys.exit(n)
