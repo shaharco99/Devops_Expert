b2d60485fe1dbc931506df9e152e1bc5ee3ea76a
Shahar <shaharco99@gmail.com>
2024-04-26 11:49:18 +0300

    remove selenium docker container

--- WorldOfGames/test.py
+++ WorldOfGames/test.py
@@ -3,12 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def test_scores_service():
-    url_score = "http://score:5000"  # Assuming 'web' is the service name of your web application container
+    url_score = "http://127.0.0.1:5000/" 
     options = Options()
     options.add_argument('--no-sandbox')
-    # options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", options=options)  # Connect to Selenium container
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(options=options)
     driver.get(url_score)
     score = int(driver.find_element(By.ID, "score").text)
     if type(score) == int:
